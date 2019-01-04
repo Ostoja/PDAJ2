c657968225996ad19e3f294f0c4370ec030e052e
MILOS <milos.ostojic747@gmail.com>
2019-01-04 14:20:52 +0100

    Distribuirana verzija dvostrukog klatna

---
+++ par.py
@@ -0,0 +1,112 @@
+import sys
+import numpy as np
+import csv
+
+import argparse
+import logging
+import os
+
+from scipy.integrate import odeint
+from multiprocessing import Pool
+
+# The gravitational acceleration (m.s-2).
+g = 9.81
+DEFAULT_RESOLUTION = 5
+DEFAULT_DT = 0.01
+DEFAULT_TMAX = 30
+
+def deriv(y, t, L1, L2, m1, m2):
+    """Return the first derivatives of y = theta1, z1, theta2, z2."""
+    theta1, z1, theta2, z2 = y
+
+    c, s = np.cos(theta1-theta2), np.sin(theta1-theta2)
+
+    theta1dot = z1
+    z1dot = (m2*g*np.sin(theta2)*c - m2*s*(L1*z1**2*c + L2*z2**2) - (m1+m2)*g*np.sin(theta1)) / L1 / (m1 + m2*s**2)
+    theta2dot = z2
+    z2dot = ((m1+m2)*(L1*z1**2*s - g*np.sin(theta2) + g*np.sin(theta1)*c) + m2*L2*z2**2*s*c) / L2 / (m1 + m2*s**2)
+    return theta1dot, z1dot, theta2dot, z2dot
+
+def solve(a):   #L1, L2, m1, m2, tmax, dt, y0
+    L1, L2, m1, m2, tmax, dt, y0 = a
+    t = np.arange(0, tmax+dt, dt)
+
+    # Do the numerical integration of the equations of motion
+    y = odeint(deriv, y0, t, args=(L1, L2, m1, m2))
+    theta1, theta2 = y[:,0], y[:,2]
+
+    # Convert to Cartesian coordinates of the two bob positions.
+    x1 = L1 * np.sin(theta1)
+    y1 = -L1 * np.cos(theta1)
+    x2 = x1 + L2 * np.sin(theta2)
+    y2 = y1 - L2 * np.cos(theta2)
+
+    return y0, theta1, theta2, x1, y1, x2, y2
+
+def make_parameters(L1, L2, m1, m2, tmax, dt, theta_resolution):
+    for theta1_init in np.linspace(0, 2*np.pi, theta_resolution):
+            for theta2_init in np.linspace(0, 2*np.pi, theta_resolution):
+                # Initial conditions: theta1, dtheta1/dt, theta2, dtheta2/dt.
+                y0 = np.array([
+                    theta1_init,
+                    0.0,
+                    theta2_init,
+                    0.0
+                ])
+                yield L1, L2, m1, m2, tmax, dt, y0
+
+def simulate_pendulum(theta_resolution, datafile, tmax, dt):
+    # Pendulum rod lengths (m), bob masses (kg).
+    L1, L2 = 1.0, 1.0
+    m1, m2 = 1.0, 1.0
+
+    # Maximum time, time point spacings (all in s).
+    #tmax, dt = 30.0, 0.01
+
+    with open(datafile, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['theta1_init', 'theta2_init', 'theta1', 'theta2', 'x1', 'y1', 'x2', 'y2'])
+        #theta1, theta2, x1, y1, x2, y2 = solve(L1, L2, m1, m2, tmax, dt, y0)
+        p = Pool()
+        rows = p.imap(solve, make_parameters(L1, L2, m1, m2, tmax, dt, theta_resolution))
+        for row in rows:
+            y0, theta1, theta2, x1, y1, x2, y2 = row
+            writer.writerow([y0[0], y0[2], theta1[-1], theta2[-1], x1[-1], y1[-1], x2[-1], y2[-1]])
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'data_file',
+        help="Data file where results are saved"
+    )
+    
+    parser.add_argument(
+        '-r',
+        '--resolution',
+        metavar='NUM',
+        type=int,
+        default=DEFAULT_RESOLUTION,
+        help="Resolution, %d by default" % DEFAULT_RESOLUTION
+    )
+    parser.add_argument(
+        '--tmax',
+        metavar='NUM',
+        type=float,
+        default=DEFAULT_TMAX,
+        help="Tmax, %f by default" % DEFAULT_TMAX
+    )
+    parser.add_argument(
+        '--dt',
+        metavar='NUM',
+        type=float,
+        default=DEFAULT_DT,
+        help="Step, %f by default" % DEFAULT_DT
+    )
+    args = parser.parse_args()
+    
+   
+    simulate_pendulum(args.resolution, args.data_file, args.tmax, args.dt)
+
+if __name__ == "__main__":
+    main()
+
---
+++ pdaj/tasks/server.py
@@ -0,0 +1,103 @@
+from datetime import datetime
+import os
+import socket
+import subprocess
+import time
+import csv
+
+from celery import chain, chord
+from celery.exceptions import Reject
+import numpy as np
+
+from ..app import app
+from .worker import simulate_pendulum_instance
+
+
+## Monitoring tasks
+
+@app.task
+def monitor_queues(ignore_result=True):
+    server_name = app.conf.MONITORING_SERVER_NAME
+    server_port = app.conf.MONITORING_SERVER_PORT
+    metric_prefix = app.conf.MONITORING_METRIC_PREFIX
+
+    queues_to_monitor = ('server', 'worker')
+    
+    output = subprocess.check_output('rabbitmqctl -q list_queues name messages consumers', shell=True)
+    lines = (line.split() for line in output.splitlines())
+    data = ((queue, int(tasks), int(consumers)) for queue, tasks, consumers in lines if queue in queues_to_monitor)
+
+    timestamp = int(time.time())
+    metrics = []
+    for queue, tasks, consumers in data:
+        metric_base_name = "%s.queue.%s." % (metric_prefix, queue)
+
+        metrics.append("%s %d %d\n" % (metric_base_name + 'tasks', tasks, timestamp))
+        metrics.append("%s %d %d\n" % (metric_base_name + 'consumers', consumers, timestamp))
+
+    sock = socket.create_connection((server_name, server_port), timeout=10)
+    sock.sendall(''.join(metrics))
+    sock.close()
+
+
+## Recording the experiment status
+
+def get_experiment_status_filename(status):
+    return os.path.join(app.conf.STATUS_DIR, status)
+
+def get_experiment_status_time():
+    """Get the current local date and time, in ISO 8601 format (microseconds and TZ removed)"""
+    return datetime.now().replace(microsecond=0).isoformat()
+
+
+@app.task
+def record_experiment_status(status):
+    with open(get_experiment_status_filename(status), 'w') as fp:
+        fp.write(get_experiment_status_time() + '\n')
+
+
+## Seeding the computations
+
+@app.task
+def simulate_pendulum(ignore_result=True):
+
+    record_experiment_status.si('started').delay()
+    L1, L2 = 1.0, 1.0
+    m1, m2 = 1.0, 1.0
+
+    THETA_RESOLUTION = app.conf.THETA_RESOLUTION
+    TIME_MAX = app.conf.TIME_MAX
+    DTIME = app.conf.DTIME
+
+    chord(
+        (simulate_pendulum_instance.s(L1, L2, m1, m2, tmax, dt, theta1_init, theta2_init)
+        for (L1, L2, m1, m2, tmax, dt, theta1_init, theta2_init) in
+        parametar_sweep(L1, L2, m1, m2, TIME_MAX, DTIME, THETA_RESOLUTION)
+        ),
+        save_pendulum.s()
+    ).delay()
+
+def parametar_sweep(L1, L2, m1, m2, tmax, dt, theta_resolution):
+    for theta1_init in np.linspace(0, 2 * np.pi, theta_resolution):
+        for theta2_init in np.linspace(0, 2 * np.pi, theta_resolution):
+            yield L1, L2, m1, m2, tmax, dt, theta1_init, theta2_init
+
+
+@app.task
+def save_pendulum(results):
+    filename = os.path.join(app.conf.RESULTS_DIR, 'results.csv')
+
+    with open(filename, 'w') as resultsfile:
+        fieldnames = ['theta1_init', 'theta2_init', 'theta1', 'theta2', 'x1', 'y1', 'x2', 'y2']
+        writer = csv.DictWriter(resultsfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for result in results:
+            theta1, theta2, x1, y1, x2, y2, y0 = result
+            writer.writerow({'theta1_init': y0[0],
+                             'theta2_init': y0[2],
+                             'theta1' : theta1[-1],
+                             'theta2' : theta2[-1],
+                             'x1' : x1[-1],
+                             'y1' : y1[-1],
+                             'x2' : x2[-1],
+                             'y2' : y2[-1]})
